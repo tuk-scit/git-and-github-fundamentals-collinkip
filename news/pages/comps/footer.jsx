b759548cef6e5543d761812527d1fef332504683
collins <collinskiplangat3>
2022-12-12 01:37:57 +0300

    one pager and prototypes renewal

---
+++ news/pages/comps/footer.jsx
@@ -0,0 +1,8 @@
+const Footer=()=>{
+    return(
+        <footer>
+            Copyright 2022 Collins Kiplangat
+        </footer>
+    )
+};
+export default Footer;
---
+++ news/pages/contact/index.jsx
@@ -0,0 +1,13 @@
+const contact = () => {
+  return (
+    <div>
+      <h1>Contact us</h1>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Reiciendis
+        distinctio incidunt aliquam beatae amet quibusdam ut nemo quisquam
+        recusandae nostrum!
+      </p>
+    </div>
+  );
+};
+export default contact;
